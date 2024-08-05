e8addb139e54b15caf65814cc21d7e0601c69c4c
juan pedroza <juan.cupapedroza@gmail.com>
2024-08-05 15:51:45 -0500

    creacon del proyecto frond y la estructura para productos

---
+++ src/app/comon/product.ts
@@ -0,0 +1,15 @@
+export class Product {
+
+constructor(
+
+  public id:number,
+  public name:String,
+  public code:String,
+  public description: String,
+  public price:number,
+  public urlImage: File,
+  public userId: String,
+  public CategoryId: String
+
+){}
+}
